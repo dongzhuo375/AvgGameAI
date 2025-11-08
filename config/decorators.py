cd9cebc7ef08f39ff8451cd5ce6795c3e049c60e
dongzhuo375 <1405528955@qq.com>
2025-11-08 18:59:35 +0800

    完善了config装饰器

--- config/decorators.py
+++ config/decorators.py
@@ -10,7 +10,6 @@ from config.config_manager import ConfigManager
 # 全局配置管理器实例
 _config_manager: Optional['ConfigManager'] = None
 
-
 def get_config_manager() -> 'ConfigManager':
     """获取配置管理器实例"""
     global _config_manager
