5fb92b38f120a34587b58ed94bb9769cc865da78
dongzhuo375 <1405528955@qq.com>
2025-11-09 19:32:32 +0800

    Init

---
+++ service/game_service.py
@@ -0,0 +1,15 @@
+from typing import Dict, Any
+
+from config.decorators import get_config_manager, config_value
+from service.story_service import init_attributes
+
+
+class GameControl:
+    def __init__(self):
+        self.configmanager = get_config_manager()
+        self.minT = self.require_story("")
+        init_attributes(self.configmanager)
+
+    def require_story(self, key):
+        return self.configmanager.get_story_value(key)
+
